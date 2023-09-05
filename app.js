5513610cac04ad2c004514ddfbda32055d27ee68
ahmad iksan <iksan2437@gmail.com>
2023-09-05 23:28:31 +0700

    add new configuration

--- app.js
+++ app.js
@@ -2,21 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// config
-const { connection } = require("./config/db");
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // route
-const users = require("./routes/users");
-const posts = require("./routes/posts");
-
-app.use("/users", users);
-app.use("/posts", posts);
-
-app.use("/", (req, res) => {
-  res.send(
-    "You are in page 404 sir, This called middleware, i don't know how this code working, but i know when i am use this code"
-  );
-});
+app.use("/users", require("./routes/users"));
+app.use("/posts", require("./routes/postRoutes"));
 
 app.listen(port, () => {
   console.log(`This app now runing using port ${port}`);
